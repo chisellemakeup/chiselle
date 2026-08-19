b1a5b2234ca9bff688f39d659cb9909d49e00a09
chisellemakeup <chisellewebsite@gmail.com>
2026-08-19 22:39:18 +0530

    update bridal makeup service page

--- src/BridalMakeupArtistPage.jsx
+++ src/BridalMakeupArtistPage.jsx
@@ -1,9 +1,16 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ServiceHeroSection from "./components/Services/ServiceHeroSection";
-import { SERVICE_PAGE_HERO_DATA, SERVICE_PAGE_WHY_US_DATA } from "./data/content";
-import ServiceAboutSection from "./components/services/ServiceAboutSection"
-import { WhyUs, SignatureStyle, HowWeWork } from "./components/services/ServiceFeaturesSection";
+import {
+  SERVICE_PAGE_HERO_DATA,
+  SERVICE_PAGE_WHY_US_DATA,
+} from "./data/content";
+import ServiceAboutSection from "./components/services/ServiceAboutSection";
+import {
+  WhyUs,
+  SignatureStyle,
+  HowWeWork,
+} from "./components/services/ServiceFeaturesSection";
 import Services from "./components/home/Services";
 import Gallery from "./components/home/Gallery";
 import ServiceFAQAccordion from "./components/services/ServiceFAQAccodion";
@@ -26,25 +33,25 @@ export default function BridalMakeupArtistPage() {
         <ServiceHeroSection content={SERVICE_PAGE_HERO_DATA[0]} />
         <ServiceAboutSection />
         <WhyUs content={SERVICE_PAGE_WHY_US_DATA} />
+        <ServicePageIntroSection2 content={SERVICE_PAGE_ABOUT_INTRO_DATA2} />
+        <Services />
         <SignatureStyle />
         <HowWeWork />
-        <Services />
-                <Gallery
-                  sectionLabel="THE CHISELLE TOUCH"
-                  sectionTitle="LOOKS THAT SPEAK FOR THEMSELVES"
-                />
-                <ServiceFAQAccordion
-        title={SERVICE_FAQ_DATA.title}
-        faq={SERVICE_FAQ_DATA.items}
-      />
-      <ServicePageIntroSection content={SERVICE_PAGE_ABOUT_INTRO_DATA} />
-      <ServicePageIntroSection2 content={SERVICE_PAGE_ABOUT_INTRO_DATA2} />
-      <Testimonials />
-              <InstaFeedSection />
-              <Contact />
-              <ServicePageAcademy content={SERVICE_PAGE_ACADEMY_DATA} />
+        <Gallery
+          sectionLabel="THE CHISELLE TOUCH"
+          sectionTitle="LOOKS THAT SPEAK FOR THEMSELVES"
+        />
+        <ServicePageIntroSection content={SERVICE_PAGE_ABOUT_INTRO_DATA} />
+        <Testimonials />
+        <ServiceFAQAccordion
+          title={SERVICE_FAQ_DATA.title}
+          faq={SERVICE_FAQ_DATA.items}
+        />
+        <InstaFeedSection />
+        <Contact />
+        <ServicePageAcademy content={SERVICE_PAGE_ACADEMY_DATA} />
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
